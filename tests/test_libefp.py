e4cd74046b517db70b2d296e0efcf3eaa6514c04
Lori A. Burns <lori.burns@gmail.com>
2017-09-04 03:39:35 -0400

    Try again 4 vs 4L

--- tests/test_libefp.py
+++ tests/test_libefp.py
@@ -57,7 +57,11 @@ def test_qm_1a():
     ptc_info = {'n': asdf.get_point_charge_count(),
                 'xyz': asdf.get_point_charge_coordinates(),
                 'val': asdf.get_point_charge_values()}
-    assert(compare_dicts({'n': 4, 'xyz': coords, 'val': ptc}, ptc_info, 6, sys._getframe().f_code.co_name + ': ptc'))
+    try:
+        nptc = long(4)
+    except SyntaxError:
+        nptc = 4
+    assert(compare_dicts({'n': nptc, 'xyz': coords, 'val': ptc}, ptc_info, 6, sys._getframe().f_code.co_name + ': ptc'))
     assert(compare_values(-0.0787829370, ene['total'], 6, sys._getframe().f_code.co_name + ': total'))
 
 
