18743a5b3a1824009905a79bf22a3aa2372e6982
Lori A. Burns <lori.burns@gmail.com>
2017-09-27 19:33:45 -0400

    Adds fake setup.py for conda versioning

---
+++ conda/_conda_vers.py
@@ -0,0 +1,34 @@
+#
+# @BEGIN LICENSE
+#
+#   conda/_conda_vers.py: dummy setup.py
+#
+#   Copyright (c) 2017 The Psi4 Developers
+#
+#   All rights reserved. Use of this source code is governed by a
+#   BSD-style license that can be found in the LICENSE file.
+#
+# @END LICENSE
+#
+
+"""Dummy setup.py file solely for the purposes of getting an on-the-fly
+computed version number into the conda recipe.
+
+"""
+import sys
+from distutils.core import setup
+
+def version_func():
+    import subprocess
+
+    command = 'python psi4/versioner.py --formatonly --format={versionlong}'
+    process = subprocess.Popen(command.split(), shell=False, stdout=subprocess.PIPE)
+    (out, err) = process.communicate()
+    if sys.version_info >= (3, 0):
+        return out.decode('utf-8').strip()
+    else:
+        return out.strip()
+
+setup(
+    version=version_func(),
+)
