f42ffb8cee33874fd1cec0c4e3dd5758c58231ec
Lori A. Burns <lori.burns@gmail.com>
2018-08-06 12:19:21 -0400

    v0.4

--- pylibefp/metadata.py
+++ pylibefp/metadata.py
@@ -1,6 +1,6 @@
-__version__ = '0.3'
-__version_long = '0.3+d56476b'
-__version_upcoming_annotated_v_tag = '0.4'
+__version__ = '0.4'
+__version_long = '0.4+zzzzzzz'
+__version_upcoming_annotated_v_tag = '0.5'
 
 def version_formatter(dummy):
     return '(inplace)'
